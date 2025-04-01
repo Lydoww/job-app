1dfc6695d64958157973359885ea1767713354f4
Alexis HELM <128365251+Lydovw@users.noreply.github.com>
2025-04-01 05:24:26 +0100

    fetch all jobs

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-
+import JobLists from "./components/JobLists";
 
 function App() {
- return(
-  <div className='text-3xl font-bold underline'>Hello world</div>
- )
-  
+  return (
+    <div className="max-w-7xl mx-auto px-4 py-8">
+      <div className="font-bold text-3xl flex justify-center mb-5">Job App</div>
+      <main>
+        <JobLists />
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/types/jobType.tsx
@@ -0,0 +1,8 @@
+interface Job {
+  id: string;
+  title: string;
+  description: string;
+  location: string;
+}
+
+export default Job;
